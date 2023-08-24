fc5a4cdb02332e343f17b4ea4703131852eebf77
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-08-24 17:26:00 -0600

    Literals and objects

---
+++ literals.py
@@ -0,0 +1,14 @@
+1 + 2
+# print(1 3.0) #     print(1 3.0) # Invalid syntax
+# print(5 / 'Platzi') # TypeError: unsupported operand type(s) for /: 'int' and 'str'
+# print(5 * 'Platzi') # PlatziPlatziPlatziPlatziPlatzi
+print('Hola, mundo!') # Hola, mundo!
+
+my_int = 1
+my_float = 1.0
+my_bool = False
+my_none = None
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_none))
